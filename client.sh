4d09928c43b3438f25128183078c1fee4141b015
sukepp <hanpulous@gmial.com>
2019-11-07 00:54:42 +0000

    1. Connect server and client. A newline bug exists.

--- client.sh
+++ client.sh
@@ -5,10 +5,16 @@ if [ $# -lt 3 ]; then
     exit 1
 fi
 
+PIPE_SERVER="server_pipe"
 CLIENT_ID=$1
+PIPE_CLIENT=$CLIENT_ID"_pipe"
 OPTION=$2
 ARGS_ARRAY=${@:3}
 ARGS_STRING=""
+
+if [ ! -e $PIPE_CLIENT ]; then
+    mkfifo $PIPE_CLIENT
+fi
 
 for value in ${ARGS_ARRAY[@]}
 do
@@ -17,7 +23,7 @@ done
 
 case "$OPTION" in
     init)
-        echo "$CLIENT_ID $OPTION $ARGS_STRING"
+        echo "$CLIENT_ID $OPTION $ARGS_STRING" > $PIPE_SERVER
         ;;
     insert)
         if [ $# -ne 4 ]; then
@@ -26,25 +32,41 @@ case "$OPTION" in
         fi
         read -p "Please write login: " login
         read -p "Please write password: " password
-        echo "$CLIENT_ID $OPTION $ARGS_STRING $login\n$password"
+        #echo "$CLIENT_ID $OPTION $ARGS_STRING $login\\n$password" > $PIPE_SERVER
+        val="$CLIENT_ID $OPTION $ARGS_STRING $login"
+        val=$val'\n'
+        val=$val"$password"
+        echo "$val" > $PIPE_SERVER
         ;;
     show)
-        echo "$CLIENT_ID $OPTION $ARGS_STRING"
+        echo "$CLIENT_ID $OPTION $ARGS_STRING" > $PIPE_SERVER
+        read exit_code < $PIPE_CLIENT
         ;;
     edit)
-        echo "$CLIENT_ID $OPTION $ARGS_STRING"
-        FILE_TEMP=`mktemp`
-        echo "login" > $FILE_TEMP
-        vim $FILE_TEMP
-        cat $FILE_TEMP
+        echo "$CLIENT_ID show $ARGS_STRING" > $PIPE_SERVER
+        read exit_code < $PIPE_CLIENT
+        if [ $exit_code -eq 0 ]; then
+            FILE_TEMP=`mktemp`
+            cat $PIPE_CLIENT > $FILE_TEMP
+            vim $FILE_TEMP
+            echo "$CLIENT_ID update $ARGS_STRING `cat $FILE_TEMP`" > $PIPE_SERVER
+            rm $FILE_TEMP
+        fi
         ;;
     rm)
-        echo "$CLIENT_ID $OPTION $ARGS_STRING"
+        echo "$CLIENT_ID $OPTION $ARGS_STRING" > $PIPE_SERVER
+        ;;
+    ls)
+        echo "$CLIENT_ID $OPTION $ARGS_STRING" > $PIPE_SERVER
         ;;
     shutdown)
-        echo "$CLIENT_ID $OPTION $ARGS_STRING"
+        echo "$CLIENT_ID $OPTION $ARGS_STRING" > $PIPE_SERVER
         ;;
     *)
+        echo "$OPTION"
         echo "Error, bad request"
         exit 1
 esac
+cat $PIPE_CLIENT
+rm $PIPE_CLIENT
+
--- server.sh
+++ server.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
+
+echo "server started."
+
+PIPE_SERVER="server_pipe"
+
+if [ ! -e $PIPE_SERVER ]; then
+   mkfifo $PIPE_SERVER
+fi
+
 while true; do
-read -p "Enter your option: " args
-option=${args%% *}
-sub_args=${args#* }
-case "$option" in
-    init)
-        ./init.sh $sub_args
-        ;;
-    insert)
-        ./insert.sh $sub_args
-        ;;
-    show)
-        ./show.sh $sub_args
-        ;;
-    update)
-        ./insert.sh $sub_args
-        ;;
-    rm)
-        ./insert.sh $sub_args
-        ;;
-    ls)
-        ./ls.sh $sub_args
-        ;;
-    shutdown)
-        exit 0
-        ;;
-    *)
-        echo "Error, bad request"
-        exit 1
-esac
+    echo "waiting for request..."
+    read args < $PIPE_SERVER
+    
+    client_id=${args%% *}
+    sub_args=${args#* }
+    option=${sub_args%% *}
+    sub_args=${sub_args#* }
+    
+    PIPE_CLIENT=$client_id"_pipe"
+    
+    case "$option" in
+        init)
+            ./init.sh $sub_args > $PIPE_CLIENT
+            ;;
+        insert)
+            echo -e $sub_args
+            ./insert.sh $sub_args > $PIPE_CLIENT
+            ;;
+        show)
+            ./show.sh $sub_args > "./tmp"
+            echo $? > $PIPE_CLIENT
+            echo `cat ./tmp` > $PIPE_CLIENT
+            rm ./tmp
+            ;;
+        update)
+            user_id=${sub_args%% *}
+            sub_args=${sub_args#* }
+            service_file=${sub_args%% *}
+            sub_args=${sub_args#* }
+            #last_arg=${sub_args##* }
+            #sub_args=${sub_args% *}
+            ./insert.sh $user_id $service_file f $sub_args > $PIPE_CLIENT
+            ;;
+        rm)
+            ./rm.sh $sub_args > $PIPE_CLIENT
+            ;;
+        ls)
+            ./ls.sh $sub_args > $PIPE_CLIENT
+            ;;
+        shutdown)
+            exit 0
+            ;;
+        *)
+            echo "Error, bad request"
+            exit 1
+    esac
 done
+
